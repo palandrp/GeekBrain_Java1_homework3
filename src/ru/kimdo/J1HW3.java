11b5fcc16c6272d4a9997e2c55e4c7dc4f8b014c
palandr <palandrp@gmail.com>
2017-05-14 23:45:08 +0500

    Добавлен функционал проверки на ранее вводимые значения. Откомментирован код.

--- src/ru/kimdo/J1HW3.java
+++ src/ru/kimdo/J1HW3.java
@@ -15,6 +15,7 @@ public class J1HW3 {
 
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
+//        Ведется счёт между циклами игр:
         int score = 0;
 
         while (true) {
@@ -22,32 +23,46 @@ public class J1HW3 {
             int number = random.nextInt(10);
             int guess = -1;
             boolean win = false;
-//            int[] result = new int[3];
-//            Arrays.fill(result, -1);
-//            System.out.println(Arrays.toString(result));
+//            Этот массив для функционала проверки, что значение
+//              еще не вводилось в игровом цикле:
+            int[] result = new int[3];
+            Arrays.fill(result, -1);
+            int game = 0;            // Счетчик попыток угадывания;
 
             while (guess < 0 || guess > 9) {
                 System.out.print("Make your choice! Input the number from 0 to 9: ");
                 guess = scanner.nextInt();
+                result[game] = guess;     // Отправляем попытку в память;
             }
-            int game = 0;
+//            System.out.println(Arrays.toString(result));       // Это отладочное сообщение;
+
+            game = 1;
             do {
                 if (guess == number){
                     System.out.println("You win!");
                     System.out.printf("It was %d!\n", number);
-                    score++;
+                    score++;               // Если выигрыш, то счет увеличивается;
                     win = true;
                     break;
                 } else if (guess > number) {
                     System.out.println("Number is less than your guess!");
-                    System.out.print("Another guess: ");
-                    guess = scanner.nextInt();
-//                    for (int a : result){
-//                        if (a == guess){
-//                            System.out.print("You said it earlier! Another guess: ");
-//                        }
-//                    }
-//                    result[game] = guess;
+                    do {
+                        System.out.print("Another guess: ");
+                        guess = scanner.nextInt();
+                    } while (guess < 0 || guess > 9);
+                    int i = 0;
+                    while (i < 3){
+                        if (result[i] == guess){
+                            System.out.println("You said it earlier!");
+                            do {
+                                System.out.print("Another guess: ");
+                                guess = scanner.nextInt();
+                            } while (guess < 0 || guess > 9);
+                            i = 0;        // Пришлось поломать голову над тем, как заставить
+                        } else i++;       // цикл, после неверного ввода проверять значения с начала;
+                    }
+                    result[game] = guess;
+//                    System.out.println(Arrays.toString(result));    // Отладочное сообщение;
                     if (guess == number){
                         System.out.println("You win!");
                         System.out.printf("It was %d!\n", number);
@@ -57,9 +72,23 @@ public class J1HW3 {
                     }
                 } else {
                     System.out.println("Number is higher than your guess!");
-                    System.out.print("Another guess: ");
-                    guess = scanner.nextInt();
-//                    result[game] = guess;
+                    do {
+                        System.out.print("Another guess: ");
+                        guess = scanner.nextInt();
+                    } while (guess < 0 || guess > 9);
+                    int i = 0;
+                    while (i < 3){
+                        if (result[i] == guess){
+                            System.out.println("You said it earlier!");
+                            do {
+                                System.out.print("Another guess: ");
+                                guess = scanner.nextInt();
+                            } while (guess < 0 || guess > 9);
+                            i = 0;
+                        } else i++;
+                    }
+                    result[game] = guess;
+//                    System.out.println(Arrays.toString(result));     // Отладочное сообщение;
                     if (guess == number){
                         System.out.println("You win!");
                         System.out.printf("It was %d!\n", number);
@@ -69,26 +98,27 @@ public class J1HW3 {
                     }
                 }
                 game++;
-            } while (game != 2);
+            } while (game != 3);
+
             if (!win) {
-                score--;
+                score--;        // Если проигрыш, то счет уменьшается (даже в минус);
                 System.out.println("You lost! Overflow numbers of attempts!");
                 System.out.printf("It was %d!\n", number);
             }
 
-            if (score == 10) {
-                System.out.println("YOU ABSOLUTELY WINNER!");
+            if (score == 10) {      // В этой игре можно победить! :)))
+                System.out.println("You are the absolute winner!");
                 System.out.println("GOODBYE!");
                 break;
             }
 
             int play = -1;
-
             while (play < 0 || play > 1) {
                 System.out.printf("Your score: %d\n", score);
                 System.out.println("Play again? Yes: 1; No: 0");
                 play = scanner.nextInt();
             }
+
             if (play == 0) {
                 System.out.print("Goodbye!");
                 break;
